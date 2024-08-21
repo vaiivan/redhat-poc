47b61cb5dfa325f3ef247d9db6e7332f71846880
vaiivan <105779710+vaiivan@users.noreply.github.com>
2024-08-21 14:38:26 +0800

    added 1st dag

---
+++ dags/test_dag.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+
+with DAG(
+    "TestDAG",
+    default_args={
+        "depends_on_past": False,
+        "retries": 0,
+        "retry_delay": timedelta(minutes=5),
+    },
+    description="A simple tutorial DAG",
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+) as dag:
+    test = KubernetesPodOperator(
+        namespace="airflow",
+        image="quay.io/eformat/airflow-runner:2.5.1",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10", "echo pwd"],
+        name="test",
+        task_id="test",
+        get_logs=True,
+    )
+
+    test
