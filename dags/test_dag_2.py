132b98a64aa8d791ffe194040731029af2c2836d
vaiivan <105779710+vaiivan@users.noreply.github.com>
2024-08-22 10:14:43 +0800

    updated test fail dag

--- dags/test_dag_2.py
+++ dags/test_dag_2.py
@@ -4,6 +4,8 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 from datetime import datetime, timedelta
 from textwrap import dedent
 
+def dag_success_alert(context):
+    print(f"DAG has succeeded, run_id: {context['run_id']}-------------")
 
 with DAG(
     "TestDAG",
@@ -15,6 +17,7 @@ with DAG(
     description="A simple tutorial DAG",
     start_date=datetime(2021, 1, 1),
     catchup=False,
+    on_success_callback=dag_success_alert,
 ) as dag:
     test = KubernetesPodOperator(
         namespace="airflow",
