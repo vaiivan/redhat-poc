d71bead1336500e31d0d94bb34c4c68f6b525647
vaiivan <105779710+vaiivan@users.noreply.github.com>
2024-08-22 10:42:38 +0800

    updated test fail dag

--- dags/a_il_test_fail_2.py
+++ dags/a_il_test_fail_2.py
@@ -3,6 +3,7 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 from textwrap import dedent
+from airflow.operators.email_operator import EmailOperator
 
 def task_failure_alert(context):
     print(f"Task has failed, task_instance_key_str:---------------------")
@@ -25,10 +26,19 @@ with DAG(
     catchup=False,
     on_failure_callback=task_failure_alert,
 ) as dag:
+    email_status = EmailOperator(
+        mime_charset='utf-8',
+        task_id="sending_status_email",
+        to="loyanngai@hotmail.com",
+        subject="Test from SES",
+        html_content="Trying to send an email from airflow through SES."
+    )
     a_il_test_fail_2 = PythonOperator(
         task_id="a_il_test_fail_2",
         python_callable=lambda: 1 / 0,
         on_failure_callback=task_failure_alert
     )
 
-    a_il_test_fail_2
+
+
+    email_status >> a_il_test_fail_2
