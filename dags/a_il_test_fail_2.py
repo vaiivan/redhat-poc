d491c047fdb1669ce1f489937111300147a43173
vaiivan <105779710+vaiivan@users.noreply.github.com>
2024-08-22 11:24:22 +0800

    added sns

--- dags/a_il_test_fail_2.py
+++ dags/a_il_test_fail_2.py
@@ -4,34 +4,39 @@ from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 from textwrap import dedent
 from airflow.operators.email_operator import EmailOperator
+from airflow.contrib.operators.sns_publish_operator import SnsPublishOperator
+
+sns_topic_arn = "arn:aws:sns:ap-east-1:695314914535:airflow-sns-tpoic"
 
 def task_failure_alert(context):
     print(f"Task has failed, task_instance_key_str:---------------------")
 
 
+def dag_check_SNS(sns_topic_arn,context):
+
+    sns = SnsPublishOperator(
+        task_id="check_list",
+        target_arn=sns_topic_arn,
+        subject="testing-subject",
+        message="testing-content",
+    )
+    sns.execute(context)
+
+
 with DAG(
     dag_id="a_il_test_fail_2",
-    # default_args={
-    #     "depends_on_past": False,
-    #     "retries": 0,
-    #     "retry_delay": timedelta(minutes=5),
-    #     "owner": "il",
-    #     # "email_on_failure": True,
-    #     # "email_on_retry": False,
-    #     # "email": ["ivan.lo@amidas.com.hk"]
-    # },
     description="DAG testing for SES",
-    # dagrun_timeout=datetime.timedelta(minutes=60),
     start_date=datetime(2021, 1, 1),
     catchup=False,
-    on_failure_callback=task_failure_alert,
+    # on_failure_callback=task_failure_alert,
 ) as dag:
     email_status = EmailOperator(
         mime_charset='utf-8',
         task_id="sending_status_email",
         to="loyanngai@hotmail.com",
         subject="Test from SES",
-        html_content="Trying to send an email from airflow through SES."
+        html_content="Trying to send an email from airflow through SES.",
+        on_failure_callback=dag_check_SNS
     )
     a_il_test_fail_2 = PythonOperator(
         task_id="a_il_test_fail_2",
