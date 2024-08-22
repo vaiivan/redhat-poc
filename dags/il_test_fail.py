1c069e917102072a75f6309b1c3bc21d2c4bbb8e
vaiivan <105779710+vaiivan@users.noreply.github.com>
2024-08-22 09:38:38 +0800

    updated test fail dag

--- dags/il_test_fail.py
+++ dags/il_test_fail.py
@@ -4,11 +4,8 @@ from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 from textwrap import dedent
 
-def il_failure_callback():
-    try:
-        print('-------------- tasked failed --------------')
-    except Exception as e:
-        raise Exception
+def task_failure_alert(context):
+    print(f"Task has failed, task_instance_key_str: {context['task_instance_key_str']}")
 
 
 with DAG(
@@ -25,7 +22,7 @@ with DAG(
     description="DAG testing for SES",
     start_date=datetime(2021, 1, 1),
     catchup=False,
-    on_failure_callback=il_failure_callback(),
+    on_failure_callback=task_failure_alert,
 ) as dag:
     IL_test_fail = PythonOperator(
         task_id="IL_test_fail",
